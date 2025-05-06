feaf0efeb3a63be5e096ac021356f6bf10259cb0
Tawhid-Ahmed <tawhidahmed98@gmail.com>
2025-05-06 18:05:34 +0600

    initial-project-commit

---
+++ src/test/java/org/tawhid/vca/VideoCallAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tawhid.vca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCallAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
